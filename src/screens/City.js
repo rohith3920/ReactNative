42fea1cf045d542f76602c41214376d6e2eaceca
rohith <rohithreddy@3920>
2024-01-04 10:20:53 +0530

    started working on the weather ui screen design

---
+++ src/screens/City.js
@@ -0,0 +1,103 @@
+import React from "react";
+import {
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  StatusBar,
+  ImageBackground,
+  View,
+} from "react-native";
+import { Feather } from "@expo/vector-icons";
+import IconText from "../components/IconText";
+
+const City = () => {
+  const {
+    container,
+    cityName,
+    cityText,
+    countryName,
+    populationWrapper,
+    populationText,
+    riseSetWrapper,
+    riseSetText,
+    rowLayout,
+    imageLayout
+  } = styles;
+  return (
+    <SafeAreaView style={container}>
+      <ImageBackground
+        source={require("../../assets/tianjin.jpg")}
+        style={imageLayout}
+      >
+        <Text style={[cityName, cityText]}>London</Text>
+        <Text style={[countryName, cityText]}>UK</Text>
+        <View style={[populationWrapper,rowLayout]}>
+          <IconText
+            iconName={"user"}
+            iconColor={"red"}
+            bodyText={"8000"}
+            bodyTextStyles={populationText}
+          />
+        </View>
+        <View style={[riseSetWrapper, rowLayout]}>
+          <IconText
+            iconName={"sunrise"}
+            iconColor={"white"}
+            bodyText={"10:46:58 am"}
+            bodyTextStyles={riseSetText}
+          />
+          <IconText
+            iconName={"sunset"}
+            iconColor={"white"}
+            bodyText={"17:28:15 pm"}
+            bodyTextStyles={riseSetText}
+          />
+        </View>
+      </ImageBackground>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: StatusBar.currentHeight || 0,
+  },
+  imageLayout: {
+    flex: 1,
+  },
+  cityName: {
+    fontSize: 40,
+  },
+  countryName: {
+    fontSize: 30,
+  },
+  cityText: {
+    color: "white",
+    fontWeight: "bold",
+    justifyContent: "center",
+    alignSelf: "center",
+  },
+  populationWrapper: {
+    justifyContent: "center",
+    marginTop: 30,
+  },
+  populationText: {
+    fontSize: 25,
+    marginLeft: 7.5,
+    color: "red",
+  },
+  riseSetWrapper: {
+    justifyContent: "space-around",
+    marginTop: 30,
+  },
+  riseSetText: {
+    fontSize: 20,
+    color: "white",
+  },
+  rowLayout: {
+    flexDirection: "row",
+    alignItems: "center",
+  },
+});
+export default City;
---
+++ src/screens/UpcomingWeather.js
@@ -0,0 +1,90 @@
+import React from "react";
+import {
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  FlatList,
+  StatusBar,
+  ImageBackground,
+} from "react-native";
+import ListItem from "../components/ListItem";
+
+const DATA = [
+  {
+    dt_txt: "2023-122-27 11:28:40",
+    main: {
+      temp_max: 8.55,
+      temp_min: 7.55,
+    },
+    weather: [
+      {
+        main: "Clear",
+      },
+    ],
+  },
+  {
+    dt_txt: "2023-122-27 12:28:40",
+    main: {
+      temp_max: 8.55,
+      temp_min: 7.55,
+    },
+    weather: [
+      {
+        main: "Clouds",
+      },
+    ],
+  },
+  {
+    dt_txt: "2023-122-27 13:28:40",
+    main: {
+      temp_max: 8.55,
+      temp_min: 7.55,
+    },
+    weather: [
+      {
+        main: "Rain",
+      },
+    ],
+  },
+];
+
+const UpcomingWeather = () => {
+  const renderItem = ({ item }) => (
+    <ListItem
+      condition={item.weather[0].main}
+      dt_txt={item.dt_txt}
+      min={item.main.temp_min}
+      max={item.main.temp_max}
+    />
+  );
+  const {container, image} = styles
+  return (
+    <SafeAreaView style={container}>
+      <Text>Upcoming Weather</Text>
+      <ImageBackground
+        source={require("../../assets/thunderstorm.jpg")}
+        style={image}
+      >
+        <FlatList
+          data={DATA}
+          renderItem={renderItem}
+          keyExtractor={(item) => item.dt_txt}
+        />
+      </ImageBackground>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: StatusBar.currentHeight || 0,
+    backgroundColor: "royalblue",
+  },
+
+  image: {
+    flex: 1,
+  },
+});
+
+export default UpcomingWeather;
